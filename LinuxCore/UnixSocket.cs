30996b75e344b47bbb96a011d694e4ae8b1d639a
Vasyl Novikov <vanoviko@microsoft.com>
2026-03-24 19:04:11 +0000

    Edits

--- LinuxCore/UnixSocket.cs
+++ LinuxCore/UnixSocket.cs
@@ -3,18 +3,18 @@ using System.Diagnostics.CodeAnalysis;
 using System.Net.Sockets;
 using System.Runtime.CompilerServices;
 
-using SocketInterop = LinuxCore.Interop.Socket;
+using static LinuxCore.Interop.Socket;
 
 namespace LinuxCore;
 
-public sealed unsafe class UnixSocket : LinuxSocketBase
+public sealed class UnixSocket : LinuxSocketBase
 {
     public UnixSocketAddress LocalAddress
     {
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         get
         {
-            GetAddress(out SocketInterop.sockaddr_un nativeAddress, out var addressLength);
+            GetAddress(out sockaddr_un nativeAddress, out var addressLength);
             return UnixSocketAddress.FromNative(nativeAddress, addressLength);
         }
     }
@@ -31,27 +31,27 @@ public sealed unsafe class UnixSocket : LinuxSocketBase
 
     public void Bind(in UnixSocketAddress address)
     {
-        address.WriteTo(out SocketInterop.sockaddr_un nativeAddress, out var addressLength);
+        address.WriteTo(out sockaddr_un nativeAddress, out var addressLength);
         Bind(in nativeAddress, addressLength);
     }
 
     public void Connect(in UnixSocketAddress address)
     {
-        address.WriteTo(out SocketInterop.sockaddr_un nativeAddress, out var addressLength);
+        address.WriteTo(out sockaddr_un nativeAddress, out var addressLength);
         Connect(in nativeAddress, addressLength);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public int SendTo(in UnixSocketAddress address, ReadOnlySpan<byte> buffer, LinuxSocketMessageFlags flags = default)
     {
-        address.WriteTo(out SocketInterop.sockaddr_un nativeAddress, out var addressLength);
+        address.WriteTo(out sockaddr_un nativeAddress, out var addressLength);
         return SendTo(in nativeAddress, addressLength, buffer, flags);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public int ReceiveFrom(out UnixSocketAddress address, Span<byte> buffer, LinuxSocketMessageFlags flags = default)
     {
-        var receivedCount = ReceiveFrom(out SocketInterop.sockaddr_un nativeAddress, out var addressLength, buffer, flags);
+        var receivedCount = ReceiveFrom(out sockaddr_un nativeAddress, out var addressLength, buffer, flags);
         address = UnixSocketAddress.FromNative(nativeAddress, addressLength);
         return receivedCount;
     }
@@ -59,14 +59,14 @@ public sealed unsafe class UnixSocket : LinuxSocketBase
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public bool TrySendTo(in UnixSocketAddress address, ReadOnlySpan<byte> buffer, out nuint sentCount, LinuxSocketMessageFlags flags = LinuxSocketMessageFlags.DontWait)
     {
-        address.WriteTo(out SocketInterop.sockaddr_un nativeAddress, out var addressLength);
+        address.WriteTo(out var nativeAddress, out var addressLength);
         return TrySendTo(in nativeAddress, addressLength, buffer, out sentCount, flags);
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public bool TryReceiveFrom(out UnixSocketAddress address, Span<byte> buffer, out nuint receivedCount, LinuxSocketMessageFlags flags = LinuxSocketMessageFlags.DontWait)
     {
-        if (TryReceiveFrom(out SocketInterop.sockaddr_un nativeAddress, out var addressLength, buffer, out receivedCount, flags))
+        if (TryReceiveFrom(out sockaddr_un nativeAddress, out var addressLength, buffer, out receivedCount, flags))
         {
             address = UnixSocketAddress.FromNative(nativeAddress, addressLength);
             return true;
