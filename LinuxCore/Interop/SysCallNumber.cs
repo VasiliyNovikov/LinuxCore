1865f4448f6b57e51ccbb120d9f214896e27b95a
Vasyl Novikov <vanoviko@microsoft.com>
2026-04-02 11:13:12 +0000

    Add missing platform support for syscall numbers

--- LinuxCore/Interop/SysCallNumber.cs
+++ LinuxCore/Interop/SysCallNumber.cs
@@ -7,14 +7,22 @@ internal abstract class SysCallNumber
 {
     public static readonly SysCallNumber Instance = RuntimeInformation.ProcessArchitecture switch
     {
-        Architecture.X64   => new X64(),
-        Architecture.Arm64 => new Arm64(),
-        _                  => throw new PlatformNotSupportedException($"Unsupported architecture: {RuntimeInformation.ProcessArchitecture}")
+        Architecture.X86 or Architecture.Arm or Architecture.Armv6 or Architecture.S390x or Architecture.Ppc64le => new Legacy(),
+        Architecture.X64                                                                                         => new X64(),
+        Architecture.Arm64 or Architecture.LoongArch64 or Architecture.RiscV64                                   => new Generic(),
+        _ => throw new PlatformNotSupportedException($"Unsupported architecture: {RuntimeInformation.ProcessArchitecture}")
     };
 
-    public abstract long SchedGetScheduler { get; }
-    public abstract long SchedSetScheduler { get; }
-    public abstract long SchedGetParam     { get; }
+    public abstract long SchedGetScheduler { get; } // __NR_sched_getscheduler
+    public abstract long SchedSetScheduler { get; } // __NR_sched_setscheduler
+    public abstract long SchedGetParam     { get; } // __NR_sched_getparam
+    
+    private sealed class Legacy : SysCallNumber
+    {
+        public override long SchedGetScheduler => 157;
+        public override long SchedSetScheduler => 156;
+        public override long SchedGetParam     => 155;
+    }
 
     private sealed class X64 : SysCallNumber
     {
@@ -23,7 +31,7 @@ internal abstract class SysCallNumber
         public override long SchedGetParam     => 143;
     }
 
-    private sealed class Arm64 : SysCallNumber
+    private sealed class Generic : SysCallNumber
     {
         public override long SchedGetScheduler => 120;
         public override long SchedSetScheduler => 119;
