b4def9ce59276dcc87cdfb17f236607cb3629a4e
Vasyl Novikov <vasiliy.novikov@outlook.com>
2026-08-03 16:07:20 +0000

    Fix

--- LinuxCore/LinuxPoll.cs
+++ LinuxCore/LinuxPoll.cs
@@ -22,7 +22,7 @@ public static unsafe class LinuxPoll
         fixed (Query* queriesPtr = queries)
             while (true)
             {
-                timeoutMilliseconds -= Math.Max((int)(LinuxClock.Monotonic - start).TotalMilliseconds, 0);
+                timeoutMilliseconds = Math.Max(timeoutMilliseconds - (int)(LinuxClock.Monotonic - start).TotalMilliseconds, 0);
                 var result = poll((pollfd*)queriesPtr, (nuint)queries.Length, timeoutMilliseconds);
                 if (!result.IsError)
                     return result > 0;
