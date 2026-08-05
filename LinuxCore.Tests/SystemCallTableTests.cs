e33f438728117da17e0af7e979e1b2900003c5a1
Vasyl Novikov <vasiliy.novikov@outlook.com>
2026-08-05 03:52:04 +0300

    Test system call table values

---
+++ LinuxCore.Tests/SystemCallTableTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinuxCore.Tests;
+
+[TestClass]
+public class SystemCallTableTests
+{
+    [TestMethod]
+    public void Current_Values_Match_Current_Platform_Headers()
+    {
+        AssertSysCall("__NR_sched_getscheduler", SystemCallTable.Current.SchedGetScheduler);
+        AssertSysCall("__NR_sched_setscheduler", SystemCallTable.Current.SchedSetScheduler);
+        AssertSysCall("__NR_sched_getparam", SystemCallTable.Current.SchedGetParam);
+    }
+
+    private static void AssertSysCall(string name, SystemCallNumber number) => Assert.AreEqual(CScript.EvaluateNInt(name, "sys/syscall.h"), number.Value);
+}
