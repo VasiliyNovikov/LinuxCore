665afde260a18bf2fd6f8142a11072eefe325f37
Vasyl Novikov <vasiliy.novikov@outlook.com>
2026-08-17 13:19:31 +0000

    Edit

--- LinuxCore.Tests/NativeConstantAssert.cs
+++ LinuxCore.Tests/NativeConstantAssert.cs
@@ -11,7 +11,7 @@ internal static class NativeConstantAssert
     public static void EnumValuesMatch<TEnum>((string Name, string Native)[] constants, params string[] headers)
         where TEnum : struct, Enum
     {
-        Assert.AreSequenceEqual(Enum.GetNames<TEnum>(), [.. constants.Select(static constant => constant.Name)], SequenceOrder.InAnyOrder);
+        Assert.AreSequenceEqual(Enum.GetNames<TEnum>(), constants.Select(static constant => constant.Name), SequenceOrder.InAnyOrder);
         var nativeValues = CScript.EvaluateInt64s([.. constants.Select(static constant => constant.Native)], headers);
         for (var i = 0; i < constants.Length; ++i)
             Assert.AreEqual(Convert.ToInt64(Enum.Parse<TEnum>(constants[i].Name), CultureInfo.InvariantCulture), nativeValues[i], constants[i].Name);
